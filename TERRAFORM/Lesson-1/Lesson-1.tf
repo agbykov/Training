4a89cf2ef3f7e26c5b318d7ed40ccce213ee2486
Alexis Bykov <agbykov@gmail.com>
2020-05-22 16:05:45 +0300

    Initial commit

---
+++ TERRAFORM/Lesson-1/Lesson-1.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_instance" "my_Ubuntu" {
+  count = 1
+  ami = "ami-0e342d72b12109f91"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "my_Ubuntu"
+    Owner = "Alexis Bykov"
+    Project = "Terraform Lessons"
+  }
+}
+
+resource "aws_instance" "my_Amazon" {
+  ami = "ami-0a9e2b8a093c02922"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "my_Amazon"
+    Owner = "Alexis Bykov"
+    Project = "Terraform Lessons"
+  }
+}
