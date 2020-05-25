b3257bf5546864ba28f0b651dc73a98f1c2b4c2c
Alexis Bykov <agbykov@gmail.com>
2020-05-25 16:49:04 +0300

    changed user-data.sh

--- TERRAFORM/Lesson-3/user-data.sh
+++ TERRAFORM/Lesson-3/user-data.sh
@@ -3,5 +3,6 @@ yum -y update
 yum -y install httpd
 myip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
 echo "<h2>WebServer with IP : $myip</h2><br>Build by Terraform using external script" > /var/www/html/index.html
+echo "<br><font color="blue">Hello World!!</font>" >> /var/www/html/index.html
 sudo service httpd start
 chkconfig httpd on
