294326bbd2cfb6aee80c6eb3a09dc196e73c814e
Alexis Bykov <agbykov@gmail.com>
2020-05-25 10:28:21 +0300

    added SSH access

--- TERRAFORM/Lesson-2/WebServer.tf
+++ TERRAFORM/Lesson-2/WebServer.tf
@@ -46,6 +46,13 @@ resource "aws_security_group" "my_webserver" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["212.103.50.156/32"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
