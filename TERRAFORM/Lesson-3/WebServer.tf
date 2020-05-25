254a1aa61ca2fffab1a01636e7342fc759a76ce9
Alexis Bykov <agbykov@gmail.com>
2020-05-25 16:45:55 +0300

    added Lesson-3 with external user-data script

--- TERRAFORM/Lesson-3/WebServer.tf
+++ TERRAFORM/Lesson-3/WebServer.tf
@@ -13,15 +13,7 @@ resource "aws_instance" "my_websever" {
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.my_webserver.id]
   key_name               = "agbykov-Frankfurt2"
-  user_data              = <<EOF
-#!/bin/bash
-yum -y update
-yum -y install httpd
-myip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
-echo "<h2>WebServer with IP : $myip</h2><br>Build by Terraform" > /var/www/html/index.html
-sudo service httpd start
-chkconfig httpd on
-EOF
+  user_data              = file("./user-data.sh")
   tags = {
     Name    = "my_Amazon"
     Owner   = "Alexis Bykov"
