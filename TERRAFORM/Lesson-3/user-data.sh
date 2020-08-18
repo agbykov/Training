2d09bd8db2d23e9ab3964952e75927a9e6498fb1
Alexis Bykov <agbykov@gmail.com>
2020-08-18 15:15:31 +0300

    changed local-ipv4 to public-ipv4

--- TERRAFORM/Lesson-3/user-data.sh
+++ TERRAFORM/Lesson-3/user-data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 yum -y update
 yum -y install httpd
-myip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
+myip=`curl http://169.254.169.254/latest/meta-data/public-ipv4`
 echo "<h2>WebServer with IP : $myip</h2><br>Build by Terraform using external script" > /var/www/html/index.html
 echo "<br><h2><font color="blue">Hello World!!</font></h2>" >> /var/www/html/index.html
 sudo service httpd start
