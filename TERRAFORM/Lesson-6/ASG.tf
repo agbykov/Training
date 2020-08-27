58c9270cf5ddd9ce8d0256c5784ea8d31804111e
Alexis Bykov <agbykov@gmail.com>
2020-08-27 16:12:11 +0300

    Lesson-6 AWS Autoscaling Group with LB

---
+++ TERRAFORM/Lesson-6/ASG.tf
@@ -0,0 +1,156 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-0c55b159cbfafe1f0"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
+  user_data       = <<-EOF
+#!/bin/bash
+echo "Hello, World" > index.html
+nohup busybox httpd -f -p ${var.server_port} &
+EOF
+}
+
+resource "aws_security_group" "instance" {
+  name        = "Dynamic Internal Web Server SG"
+  description = "Dynamic Security Group open SSH and Web Server ports"
+
+  dynamic "ingress" {
+    for_each = ["${var.server_port}", "22"]
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name    = "Dynamic SG allow_webserver HTTP and SSH"
+    Owner   = "Alexis Bykov"
+    Project = "Terraform Lessons"
+  }
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier  = data.aws_subnet_ids.default.ids
+
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
+
+  min_size = 2
+  max_size = 10
+
+  tags = [
+    {
+      key                 = "Name"
+      value               = "terraform-asg-example"
+      propagate_at_launch = true
+    },
+    {
+      key                 = "Owner"
+      value               = "Alexis Bykov"
+      propagate_at_launch = true
+    },
+    {
+      key                 = "Project"
+      value               = "Terraform Lessons"
+      propagate_at_launch = true
+    }
+  ]
+
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_lb" "example" {
+  name               = "terraform-asg-example"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnet_ids.default.ids
+  security_groups    = [aws_security_group.alb.id]
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = 80
+  protocol          = "HTTP"
+  # By default retutn empty page with 404 code
+  default_action {
+    type = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
+
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb"
+  # Allow all incoming HTTP-requests
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  # All all outcoming requests
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb_target_group" "asg" {
+  name     = "terraform-asg-example"
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
+
